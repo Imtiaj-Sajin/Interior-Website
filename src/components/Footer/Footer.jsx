f72510f4ebf9ff84d770e042ce3d551d80bc44e2
Imtiaj Sajin <imtiajsajin@gmail.com>
2025-12-17 13:12:52 +0600

    Update email address in Footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -16,7 +16,7 @@ const Footer = () => {
             </div>
             <div className="footer-About-item">
               <AiOutlineMail className="footer-About-icon" />
-              <span>info@buenavistainteriors.es</span>
+              <span>info@vistainteriors.es</span>
             </div>
             <div className="footer-About-item">
               <AiOutlineHome className="footer-About-icon" />
